057ac98014e9c3737e6fe2d3e86f921392852eee
Howard Knorr <knorr.h@gmercyu.edu>
2022-02-23 11:12:20 -0500

    Add files via upload

---
+++ src/main/main/java/frc/robot/Constants.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    //Speeds 
+    public static final double RAMP_RATE = 0.75;
+    public static final double MAX_SPEED = 0.65;
+    public static final double SHOOTER_HIGH_SPEED = 1.0;
+    public static final double SHOOTER_LOW_SPEED = 0.5;
+    public static final double INTAKE_SPEED = 0.75;
+
+    //Can bus addresses for Drivetrain Motors
+    public static final int TOP_LEFT     = 10; 
+    public static final int BOTTOM_LEFT  = 11;
+    public static final int TOP_RIGHT    = 20; 
+    public static final int BOTTOM_RIGHT = 21;
+    // PORTS
+    public static final int encoder_LeftA = 6;
+    public static final int encoder_RightA = 8;
+    public static final int encoder_LeftB = 7;
+    public static final int encoder_RightB = 9;
+    public static final int SHOOTER_PORT = 30;
+    public static final int INTAKE_PORT = 31;
+    public static final int GATE_PORT = 0;
+    public static final int ULTRASONIC_PORT = 0;
+    public static final int GYRO_PORT = 1;
+
+    public static final double ENCODER_PPR = 360.0; //360 CPR * pulses/rev Pratice Bot
+    public static final double WHEEL_DIAM = 6.0; //inches Pratice Bot
+    public static final double DIST_ADJUSTMENT = 5.0; 
+
+
+}
---
+++ src/main/main/java/frc/robot/commands/GetWithinDistance.java
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class GetWithinDistance extends CommandBase {
+  //Class Global VAriaables to store the Subsystem and Speed/Distance values
+  //  provided during command creation through the Constructor
+  private Drivetrain dt; //Stored drivetrain subsystem
+  private double s;      //Stored Speed value
+  private double d;      //Stored Distance to travel
+
+  /** Creates a new GetWithinDistance. */
+  public GetWithinDistance(Drivetrain drivetrain, double speed, double distance) {
+    //Save the parameters in the Global Variables
+    dt = drivetrain;
+    s = speed;
+    d = distance;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    //Stop the Robot from moving
+    dt.stop();
+    //If we are to close, set speed to go in reverrse
+    if (dt.getDistance() < d){
+      s = -s;
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //Drive the robot at the desired speed using Tank Drive method
+    dt.TeleopTankDrive(s, s);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    //Stop the Robot
+    dt.stop();
+    //Reset the Global Speed to positive (Initial Condition)
+    if (s < 0){
+      s = -s;
+    }
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    //If the Robot is moving forward stop when sensor distance is <= d
+    if ((s >= 0) && (dt.getDistance() <= d)){
+      dt.stop();
+      return true;
+    //If the Robot is moving backward stop when sensor distance is >= d
+    }else if ((s <= 0) && (dt.getDistance() >= d)){
+      dt.stop();
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
